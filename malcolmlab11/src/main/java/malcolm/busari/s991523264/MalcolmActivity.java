fdc18b529d80afc93a0129040f8c27e75db4d099
Malcolm Busari <busarim@shernet.sheridancollege.ca>
2021-01-29 20:44:46 -0500

    Initial commit

---
+++ malcolmlab11/src/main/java/malcolm/busari/s991523264/MalcolmActivity.java
@@ -0,0 +1,19 @@
+/*
+Malcolm Busari s991523264
+This is assignment 1 to demonstrate how the use of activities
+and third-party apps
+ */
+package malcolm.busari.s991523264;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MalcolmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
